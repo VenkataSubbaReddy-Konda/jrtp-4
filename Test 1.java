2ffdbfd6dfe7e62e872ab76f7ae5041e7d448583
venkatasubbareddykonda <subbareddykonda667@gmail.com>
2026-05-01 11:19:46 +0530

    all files added

---
+++ Test 1.java
@@ -0,0 +1,17 @@
+	      git add                 git commit	git push
+Working tree ---------> Staging area ---------> Local Repo ------> Central Repo
+  git init					git log	
+  git status    
+
+
+
+
+Git commands:
+
+git init	: To initialize the working tree
+			git init
+
+git status	: To check the working tree status(staged + unstaged files)
+			git status
+
+git add		: Add files to staging area
---
+++ Test 3.java
@@ -0,0 +1,11 @@
+	      git add                 git commit	git push
+Working tree ---------> Staging area ---------> Local Repo ------> Central Repo
+  git init					git log	
+  git status    
+
+
+
+
+Git commands:
+
+many commands
